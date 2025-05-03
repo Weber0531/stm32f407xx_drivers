fe32ce09d9e1d384f4768107602bea292b6b3fa2
Weber0531 <ooooppppweber@gmail.com>
2025-05-03 15:25:56 +0800

    update IRQ interrupt and priority function

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -11,6 +11,46 @@
 #include <stdint.h>
 #define __vo volatile
 
+
+/**********************************START:Processor Specific Details **********************************/
+/*
+ * ARM Cortex Mx Processor NVIC ISERx register Addresses
+ */
+#define NVIC_ISER0			((__vo uint32_t*)0xE000E100)
+#define NVIC_ISER1			((__vo uint32_t*)0xE000E104)
+#define NVIC_ISER2			((__vo uint32_t*)0xE000E108)
+#define NVIC_ISER3			((__vo uint32_t*)0xE000E10C)
+#define NVIC_ISER4			((__vo uint32_t*)0xE000E110)
+#define NVIC_ISER5			((__vo uint32_t*)0xE000E114)
+#define NVIC_ISER6			((__vo uint32_t*)0xE000E118)
+#define NVIC_ISER7			((__vo uint32_t*)0xE000E11C)
+
+
+/*
+ * ARM Cortex Mx Processor NVIC ICERx register Addresses
+ */
+#define NVIC_ICER0			((__vo uint32_t*)0xE000E180)
+#define NVIC_ICER1			((__vo uint32_t*)0xE000E184)
+#define NVIC_ICER2			((__vo uint32_t*)0xE000E188)
+#define NVIC_ICER3			((__vo uint32_t*)0xE000E18C)
+#define NVIC_ICER4			((__vo uint32_t*)0xE000E190)
+#define NVIC_ICER5			((__vo uint32_t*)0xE000E194)
+#define NVIC_ICER6			((__vo uint32_t*)0xE000E198)
+#define NVIC_ICER7			((__vo uint32_t*)0xE000E19C)
+
+
+/*
+ * ARM Cortex Mx Processor Priority Register Address Calculation
+ */
+#define NVIC_PR_BASEADDR	((__vo uint32_t*)0xE000E400)
+
+
+/*
+ * ARM Cortex Mx Processor number of priority bits implemented in Priority Register
+ */
+#define NO_PR_BITS_IMPLEMENTED	4
+
+
 /*
  *  The base address of flash and SRAM memories
  */
